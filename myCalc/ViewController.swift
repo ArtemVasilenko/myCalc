11d9c32457ad4f68e804fe0d68e38af8703ba184
ArtemVasilenko <artemvasilenko88@gmail.com>
2018-12-10 20:50:07 +0200

    Initial Commit

---
+++ myCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCalc
+//
+//  Created by Артем on 12/10/18.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
